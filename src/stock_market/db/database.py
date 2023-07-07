6d3b2f76e065fd216b5670365f8bbee6c127f517
Leandro Ramos <Leandro.Ramos@intellisite.io>
2023-07-07 12:30:52 -0300

    [first version] Remove useless line

--- src/stock_market/db/database.py
+++ src/stock_market/db/database.py
@@ -7,7 +7,6 @@ from sqlalchemy_utils import create_database, database_exists
 from stock_market.settings import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-
 if not database_exists(engine.url):
     create_database(engine.url)
 
